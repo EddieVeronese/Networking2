e7be38de9373e74fe5b5f00720b928dfdf9ba97d
EddieVeronese <eddieveronese1928@libero.it>
2023-04-30 22:54:19 +0200

    Test stra easy

--- project_files_eddie/controller.py
+++ project_files_eddie/controller.py
@@ -59,7 +59,7 @@ class TrafficSlicing(app_manager.RyuApp):
             1: {2:1, 2:4},
             2: {2:1, 3:4},
             3: {2:1},
-            3: {2:1}
+            4: {2:1}
             
         }
         
