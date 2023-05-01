29c9f04dc490fc8b51fc5f80c4d9db501e4b4763
EddieVeronese <eddieveronese1928@libero.it>
2023-05-01 16:42:49 +0200

    Test 13

--- project_files_eddie/default.sh
+++ project_files_eddie/default.sh
@@ -13,14 +13,15 @@ queues:12=@1q queues:34=@1q -- \
 --id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
 
 # Switch 2
-printf "\nSwitch 2\n"
+printf "Switch 2\n"
 sudo ovs-vsctl -- \
 set port s2-eth1 qos=@newqos -- \
 set port s2-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=200000000 \
-queues:12=@1q -- \
---id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
+queues:56=@1q queues:78=@1q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000 -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
 
 # Switch 3
 printf "\nSwitch 3\n"
@@ -29,7 +30,7 @@ set port s3-eth1 qos=@newqos -- \
 set port s3-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=200000000 \
-queues:34=@1q -- \
+queues:12=@1q -- \
 --id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
 
 # Switch 4
@@ -37,44 +38,94 @@ printf "\nSwitch 4\n"
 sudo ovs-vsctl -- \
 set port s4-eth1 qos=@newqos -- \
 set port s4-eth2 qos=@newqos -- \
+set port s4-eth3 qos=@newqos -- \
+set port s4-eth4 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:34=@1q queues:56=@1q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000 -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
+
+
+# Switch 5
+printf "\nSwitch 5\n"
+sudo ovs-vsctl -- \
+set port s5-eth1 qos=@newqos -- \
+set port s5-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:78=@1q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
+
+# Switch 6
+printf "\nSwitch 6\n"
+sudo ovs-vsctl -- \
+set port s6-eth1 qos=@newqos -- \
+set port s6-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=200000000 \
 queues:12=@1q queues:34=@1q -- \
 --id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000 -- \
 --id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
 
+# Switch 7
+printf "\nSwitch 7\n"
+sudo ovs-vsctl -- \
+set port s7-eth1 qos=@newqos -- \
+set port s7-eth2 qos=@newqos -- \
+--id=@newqos create QoS type=linux-htb \
+other-config:max-rate=200000000 \
+queues:56=@1q queues:78=@1q -- \
+--id=@1q create queue other-config:min-rate=1000000 other-config:max-rate=100000000 -- \
+--id=@2q create queue other-config:min-rate=1000000 other-config:max-rate=100000000
+
+
 # Creating links
 printf "\n[INFO] Creating links..."
 
 # Switch 1
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:12,output:1
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:3
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:12,output:1
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:4
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=5,idle_timeout=0,actions=drop
-
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:34,output:2
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:4
-
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:34,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:3
 
 # Switch 2
-sudo ovs-ofctl add-flow s2 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:2
-sudo ovs-ofctl add-flow s2 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:12,output:1
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:56,output:1
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:56,output:4
+
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:78,output:2
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:78,output:3
 
 # Switch 3
-sudo ovs-ofctl add-flow s3 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:34,output:2
-sudo ovs-ofctl add-flow s3 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:1
+sudo ovs-ofctl add-flow s3 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:2
+sudo ovs-ofctl add-flow s3 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:12,output:1
 
-# Switch 4
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:3
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:12,output:1
+#Switch 4 
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:34,output:2
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:1
 
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:56,output:3
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:56,output:4
 
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:4
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:34,output:2
+# Switch 5
+sudo ovs-ofctl add-flow s5 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:78,output:2
+sudo ovs-ofctl add-flow s5 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:78,output:1
 
-sudo ovs-ofctl add-flow s4 ip,priority=65500,in_port=6,idle_timeout=0,actions=drop
+# Switch 6
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:12,output:4
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:12,output:1
+
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:3
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:34,output:2
+
+# Switch 7
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:56,output:4
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:56,output:1
+
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:78,output:3
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:78,output:2
+
 
 
 
--- project_files_eddie/topology.py
+++ project_files_eddie/topology.py
@@ -17,7 +17,7 @@ class NetworkSlicingTopo(Topo):
         host_link_config = dict()
 
         #creo 7 switch
-        for i in range(4):
+        for i in range(7):
             sconfig = {"dpid": "%016x" % (i + 1)}
             self.addSwitch("s%d" % (i + 1), **sconfig)
 
@@ -26,19 +26,23 @@ class NetworkSlicingTopo(Topo):
             self.addHost("h%d" % (i + 1), **host_config) 
             
  
-        self.addLink('s1', 's2')
         self.addLink('s1', 's3')
+        self.addLink('s1', 's4')
         self.addLink('s2', 's4')
-        self.addLink('s3', 's4')
+        self.addLink('s2', 's5')
+        self.addLink('s3', 's6')
+        self.addLink('s4', 's6')
+        self.addLink('s4', 's7')
+        self.addLink('s5', 's7')
 
         self.addLink('h1', 's1')
-        self.addLink('h2', 's4')
+        self.addLink('h2', 's6')
         self.addLink('h3', 's1')
-        self.addLink('h4', 's4')
-        self.addLink('h5', 's1')
-        self.addLink('h6', 's4')
-        self.addLink('h7', 's1')
-        self.addLink('h8', 's4')
+        self.addLink('h4', 's6')
+        self.addLink('h5', 's2')
+        self.addLink('h6', 's7')
+        self.addLink('h7', 's2')
+        self.addLink('h8', 's7')
 
 
 topos = {"networkslicingtopo": (lambda: NetworkSlicingTopo())}
@@ -61,8 +65,6 @@ if __name__ == "__main__":
     net.build()
     net.start()
 
-    subprocess.call("./prova.sh")
-    time.sleep(30)
     subprocess.call("./default.sh")
 
     
