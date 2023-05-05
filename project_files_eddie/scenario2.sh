f6e8ee5f1dec5df6b34f0decba9061ecc69c3d49
EddieVeronese <eddieveronese1928@libero.it>
2023-05-05 10:07:37 +0200

    Minor chnages

--- project_files_eddie/scenario2.sh
+++ project_files_eddie/scenario2.sh
@@ -8,7 +8,7 @@ set port s1-eth1 qos=@newqos -- \
 set port s1-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:12=@1q queues:34=@2q queues:9a=@3q queues:bc=@4q -- \
+queues:12=@1q queues:34=@2q queues:910=@3q queues:1011=@4q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@2q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@3q create queue other-config:min-rate=100000 other-config:max-rate=300000 -- \
@@ -32,7 +32,7 @@ set port s3-eth1 qos=@newqos -- \
 set port s3-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000 \
-queues:12=@1q queues:9a=@2q-- \
+queues:12=@1q queues:910=@2q-- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@2q create queue other-config:min-rate=100000 other-config:max-rate=300000
 
@@ -45,7 +45,7 @@ set port s4-eth3 qos=@newqos -- \
 set port s4-eth4 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:34=@1q queues:bc=@2q queues:56=@3q -- \
+queues:34=@1q queues:1011=@2q queues:56=@3q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@2q create queue other-config:min-rate=100000 other-config:max-rate=300000 -- \
 --id=@3q create queue other-config:min-rate=100000 other-config:max-rate=1000000
@@ -68,7 +68,7 @@ set port s6-eth1 qos=@newqos -- \
 set port s6-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:12=@1q queues:34=@2q queues:9a=@3q queues:bc=@4q -- \
+queues:12=@1q queues:34=@2q queues:910=@3q queues:1011=@4q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@2q create queue other-config:min-rate=100000 other-config:max-rate=700000 -- \
 --id=@3q create queue other-config:min-rate=100000 other-config:max-rate=300000 -- \
@@ -96,11 +96,11 @@ sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,idle_timeout=0,acti
 sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:2
 sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.4,idle_timeout=0,actions=set_queue:34,output:3
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:9a,output:1
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:9a,output:5
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:910,output:1
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:910,output:5
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:bc,output:2
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.12,idle_timeout=0,actions=set_queue:bc,output:6
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:1011,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.12,idle_timeout=0,actions=set_queue:1011,output:6
 
 # Switch 2 =
 sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.5,idle_timeout=0,actions=set_queue:56,output:1
@@ -113,8 +113,8 @@ sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.8,idle_timeout=0,acti
 sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,actions=set_queue:12,output:2
 sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.2,idle_timeout=0,actions=set_queue:12,output:1
 
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:9a,output:2
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:9a,output:1
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:910,output:2
+sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:910,output:1
 
 #Switch 4 ok
 sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:2
@@ -133,11 +133,11 @@ sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.1,idle_timeout=0,acti
 sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.4,idle_timeout=0,actions=set_queue:34,output:2
 sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.3,idle_timeout=0,actions=set_queue:34,output:3
 
-sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:9a,output:1
-sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:9a,output:5
+sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.10,idle_timeout=0,actions=set_queue:910,output:1
+sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.9,idle_timeout=0,actions=set_queue:910,output:5
 
-sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.12,idle_timeout=0,actions=set_queue:bc,output:2
-sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.11,idle_timeout=0,actions=set_queue:bc,output:6
+sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.12,idle_timeout=0,actions=set_queue:1011,output:2
+sudo ovs-ofctl add-flow s6 ip,priority=65500,nw_src=10.0.0.11,idle_timeout=0,actions=set_queue:1011,output:6
 
 
 # Switch 5 =
