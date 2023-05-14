6f2a01a4d33dbac279646faac43c32d05f50b96c
fed4z <fedi.adami@gmail.com>
2023-05-14 16:32:36 +0200

    naings fixed

--- scripts/scenario4.sh
+++ scripts/scenario4.sh
@@ -6,24 +6,24 @@ do
     sudo ovs-ofctl del-flows $bridge
 done
 
-# Switch 1 2000 Mb  ->for blue_slice
+# Switch 1 2000 Mb  ->for 1_slice
 printf "Switch 1\n"
 sudo ovs-vsctl -- \
 set port s1-eth1 qos=@newqos -- \
 set port s1-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:blue=@1q -- \
+queues:1=@1q -- \
 --id=@1q create queue other-config:min-rate=200000 other-config:max-rate=2000000
 
-# Switch 2 2000 Mb ->for orange_slice
+# Switch 2 2000 Mb ->for 2_slice
 printf "Switch 2\n"
 sudo ovs-vsctl -- \
 set port s2-eth1 qos=@newqos -- \
 set port s2-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:orange=@1q -- \
+queues:2=@1q -- \
 --id=@1q create queue other-config:min-rate=200000 other-config:max-rate=2000000
 
 # Switch 3 ->not used in this config
@@ -34,7 +34,7 @@ set port s3-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000
 
-# Switch 4 ->both for blue and orange _slices
+# Switch 4 ->both for 1 and 2 _slices
 printf "\nSwitch 4\n"
 sudo ovs-vsctl -- \
 set port s4-eth1 qos=@newqos -- \
@@ -43,7 +43,7 @@ set port s4-eth3 qos=@newqos -- \
 set port s4-eth4 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:blue=@1q queues:orange=@2q -- \
+queues:1=@1q queues:2=@2q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=1000000 -- \
 --id=@2q create queue other-config:min-rate=100000 other-config:max-rate=1000000
 
@@ -56,24 +56,24 @@ set port s5-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000
 
-# Switch 6 ->for orange_slice
+# Switch 6 ->for 2_slice
 printf "\nSwitch 6\n"
 sudo ovs-vsctl -- \
 set port s6-eth1 qos=@newqos -- \
 set port s6-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:orange=@1q -- \
+queues:2=@1q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=1000000
 
-# Switch 7 ->for blue_slice
+# Switch 7 ->for 1_slice
 printf "\nSwitch 7\n"
 sudo ovs-vsctl -- \
 set port s7-eth1 qos=@newqos -- \
 set port s7-eth2 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=2000000 \
-queues:blue=@1q -- \
+queues:1=@1q -- \
 --id=@1q create queue other-config:min-rate=100000 other-config:max-rate=1000000
 
 
@@ -81,22 +81,22 @@ queues:blue=@1q -- \
 printf "\n[INFO] Creating links..."
 
 # Switch 1
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:blue,output:2
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:blue,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:1,output:2
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:1,output:2
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:blue,output:4
-sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:blue,output:3
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:1,output:4
+sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:1,output:3
 
 sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=5,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=6,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow s1 ip,priority=65500,in_port=1,idle_timeout=0,actions=drop
 
 # Switch 2
-sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:orange,output:1
-sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:orange,output:1
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:2,output:1
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:2,output:1
 
-sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:orange,output:4
-sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:orange,output:3
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:2,output:4
+sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:2,output:3
 
 sudo ovs-ofctl add-flow s2 ip,priority=65500,in_port=2,idle_timeout=0,actions=drop
 
@@ -105,19 +105,19 @@ sudo ovs-ofctl add-flow s3 ip,priority=65500,in_port=1,idle_timeout=0,actions=dr
 sudo ovs-ofctl add-flow s3 ip,priority=65500,in_port=2,idle_timeout=0,actions=drop
 
 #Switch 4 
-sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:blue,output:3
-sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:blue,output:1
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:1,output:3
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:1,output:1
 
-sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:orange,output:2
-sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:orange,output:4
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:2,output:2
+sudo ovs-ofctl add-flow s4 table=0,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:2,output:4
 
 # Switch 5
 sudo ovs-ofctl add-flow s5 ip,priority=65500,in_port=1,idle_timeout=0,actions=drop
 sudo ovs-ofctl add-flow s5 ip,priority=65500,in_port=2,idle_timeout=0,actions=drop
 
 # Switch 6
-sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:orange,output:2
-sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:orange,output:2
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:2,output:2
+sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:2,output:2
 
 sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:4
 sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=2,idle_timeout=0,actions=set_queue:34,output:3
@@ -126,8 +126,8 @@ sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=5,idle_timeout=0,actions=dr
 sudo ovs-ofctl add-flow s6 ip,priority=65500,in_port=6,idle_timeout=0,actions=drop
 
 # Switch 7
-sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:blue,output:1
-sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:blue,output:1
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=4,idle_timeout=0,actions=set_queue:1,output:1
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=3,idle_timeout=0,actions=set_queue:1,output:1
 
-sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:blue,output:4
-sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:blue,output:3
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:1,output:4
+sudo ovs-ofctl add-flow s7 ip,priority=65500,in_port=1,idle_timeout=0,actions=set_queue:1,output:3
