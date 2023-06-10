395ee2514ebc3bdd4a4089e3b493bfbe0148bc52
fed4z <fedi.adami@gmail.com>
2023-06-10 15:38:52 +0200

    disble current scenario button

--- scripts/gui.py
+++ scripts/gui.py
@@ -17,22 +17,24 @@ scenario3_slice2_on = False
 scenario3_slice3_on = False
 scenario3_slice4_on = False
 
+# to see GUI behviour but not actually activating slices set TRUE else FALSE
+DEBUG = True
 
 class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
 
-        self.geometry("1180x880")
+        self.geometry("1000x700")
         self.grid_rowconfigure(1, weight=1)  # configure grid system
         self.grid_rowconfigure((2,3), weight=1)
         self.grid_columnconfigure((0,1,2), weight=1)
 
         central_frame = ctk.CTkFrame(self , corner_radius=0)
-        central_frame.grid(row=0, column=1, rowspan=3, columnspan=3, sticky="nswe" )
+        central_frame.grid(row=0, column=1, rowspan=3, columnspan=2, padx=0, pady=0, sticky="e")
 
         # inserting the image
-        self.canvas= Canvas(central_frame, width= 500, height= 350)
+        self.canvas= Canvas(central_frame, width= 850, height= 650)
         self.canvas.pack(side="right", fill="both", expand=True)
 
         # add riferimento locale
@@ -42,42 +44,48 @@ class App(ctk.CTk):
         self.canvas.create_image(5,5,anchor=NW,image=self.rete_img)
 
         self.arr_lines = [None]*35
-        left_frame =  ctk.CTkFrame(self, width=140, height=800, corner_radius=0)
+        left_frame =  ctk.CTkFrame(self, width=100, height=200, corner_radius=0)
         left_frame.grid(row=0, column=0, rowspan=3, columnspan=2, sticky="nsw")
-        left_frame.grid_rowconfigure((1,2,3,4,5), weight=1)
+        left_frame.grid_rowconfigure((1,2,3,4,5), weight=0)
 
-        bottom_frame =  ctk.CTkFrame(self, width=1000, height=50,  corner_radius=0)
-        bottom_frame.grid(row=3, column=0, rowspan=3, columnspan=3, sticky="swe")
-        bottom_frame.grid_columnconfigure((1,2,3,4), weight=1)
+        bottom_frame =  ctk.CTkFrame(self, width=1000, height=10,  corner_radius=0)
+        bottom_frame.grid(row=3, column=0, rowspan=3, columnspan=3, sticky="e")
+        bottom_frame.grid_columnconfigure((1,2,3,4), weight=0)
 
-        btn_scenarioD = ctk.CTkButton(left_frame, text="default", command=button_scenarioD)
-        btn_scenario1 = ctk.CTkButton(left_frame, text="scenario1", command=button_scenario1)
-        btn_scenario2 = ctk.CTkButton(left_frame, text="scenario2", command=button_scenario2)
-        btn_scenario3 = ctk.CTkButton(left_frame, text="scenario3", command=button_scenario3)
-        btn_scenario4 = ctk.CTkButton(left_frame, text="scenario4", command=button_scenario4)
+        self.btn_scenarioD = ctk.CTkButton(left_frame, text="default", command=button_scenarioD)
+        self.btn_scenario1 = ctk.CTkButton(left_frame, text="scenario1", command=button_scenario1)
+        self.btn_scenario2 = ctk.CTkButton(left_frame, text="scenario2", command=button_scenario2)
+        self.btn_scenario3 = ctk.CTkButton(left_frame, text="scenario3", command=button_scenario3)
+        self.btn_scenario4 = ctk.CTkButton(left_frame, text="scenario4", command=button_scenario4)
 
-        btn_scenarioD.grid(row=1, column=0, padx=20, pady=10)
-        btn_scenario1.grid(row=2, column=0, padx=20, pady=10)
-        btn_scenario2.grid(row=3, column=0, padx=20, pady=10)
-        btn_scenario3.grid(row=4, column=0, padx=20, pady=10)
-        btn_scenario4.grid(row=5, column=0, padx=20, pady=10)
+        self.btn_scenarioD.grid(row=1, column=0, padx=0, pady=40)
+        self.btn_scenario1.grid(row=2, column=0, padx=0, pady=40)
+        self.btn_scenario2.grid(row=3, column=0, padx=0, pady=40)
+        self.btn_scenario3.grid(row=4, column=0, padx=0, pady=40)
+        self.btn_scenario4.grid(row=5, column=0, padx=0, pady=40)
 
         self.sub_button1 = ctk.CTkButton(bottom_frame, text="slice1", command=button_scenario3_slice1, state= DISABLED)
         self.sub_button2 = ctk.CTkButton(bottom_frame, text="slice2", command=button_scenario3_slice2, state= DISABLED)
         self.sub_button3 = ctk.CTkButton(bottom_frame, text="slice3", command=button_scenario3_slice3, state= DISABLED)
         self.sub_button4 = ctk.CTkButton(bottom_frame, text="slice4", command=button_scenario3_slice4, state= DISABLED)
 
-        self.sub_button1.grid(row=0, column=0, padx=10, pady=20)
-        self.sub_button2.grid(row=0, column=1, padx=10, pady=20)
-        self.sub_button3.grid(row=0, column=2, padx=10, pady=20)
-        self.sub_button4.grid(row=0, column=3, padx=10, pady=20)
+        self.sub_button1.grid(row=0, column=0, padx=10, pady=10)
+        self.sub_button2.grid(row=0, column=1, padx=10, pady=10)
+        self.sub_button3.grid(row=0, column=2, padx=10, pady=10)
+        self.sub_button4.grid(row=0, column=3, padx=10, pady=10)
+
+        
        
 
 def button_scenarioD():
     disble_bottom_btn()
     clear_lines()
+    enable_side_btn()
+    app.btn_scenarioD.configure(state = DISABLED)
+
     ## DEFAULT SCENARIO
-    subprocess.call("./default.sh")	
+    if DEBUG != True:
+        subprocess.call("./default.sh")	
 
     #lines between hosts and switches
     app.arr_lines[0] = h11_s1_line = app.canvas.create_line(260,130,260,200, fill="", width=5)
@@ -113,8 +121,11 @@ def button_scenarioD():
 def button_scenario1():
     disble_bottom_btn()
     clear_lines()
+    enable_side_btn()
+    app.btn_scenario1.configure(state= DISABLED)
     ## SCENARIO 1
-    subprocess.call("./scenario1.sh")
+    if DEBUG != True:
+        subprocess.call("./scenario1.sh")
 
     #lines between hosts and switches
     app.arr_lines[0] = h11_s1_line = app.canvas.create_line(260,130,260,200, fill="", width=5)
@@ -149,8 +160,11 @@ def button_scenario1():
 def button_scenario2():
     disble_bottom_btn()
     clear_lines()
+    enable_side_btn()
+    app.btn_scenario2.configure(state= DISABLED)
     ## SCENARIO 2 
-    subprocess.call("./scenario2.sh")
+    if DEBUG != True:
+        subprocess.call("./scenario2.sh")
 
     #lines between hosts and switches
     app.arr_lines[0] = h11_s1_line = app.canvas.create_line(260,130,260,200, fill="purple", width=5)
@@ -191,6 +205,8 @@ def button_scenario3():
 
     clear_lines()
     enable_bottom_btn()
+    enable_side_btn()
+    app.btn_scenario3.configure(state= DISABLED)
 
 
 def button_scenario3_slice1():
@@ -302,13 +318,16 @@ def button_scenario3_slice4():
 def button_scenario4():
     disble_bottom_btn()
     clear_lines()
+    enable_side_btn()
+    app.btn_scenario4.configure(state= DISABLED)
     ## SCENARIO 4
-    subprocess.call("./scenario4.sh")
+    if DEBUG != True:
+        subprocess.call("./scenario4.sh")
     
     #lines between hosts and switches
-    app.arr_lines[0] = h11_s1_line = app.canvas.create_line(260,130,260,200, fill="blue", width=5)
+    app.arr_lines[0] = h11_s1_line = app.canvas.create_line(260,130,260,200, fill="", width=5)
 
-    app.arr_lines[1] = h9_s1_line = app.canvas.create_line(170,130,220,200, fill="blue", width=5)
+    app.arr_lines[1] = h9_s1_line = app.canvas.create_line(170,130,220,200, fill="", width=5)
     app.arr_lines[2] = h1_s1_line = app.canvas.create_line(130,190,200,220, fill="blue", width=5)
     app.arr_lines[3] = h3_s1_line = app.canvas.create_line(130,290,200,250, fill="blue", width=5)
 
@@ -316,9 +335,9 @@ def button_scenario4():
     app.arr_lines[5] = h7_s2_line = app.canvas.create_line(130,540,200,510, fill="yellow", width=5)
 
     
-    app.arr_lines[6] = h10_s6_line = app.canvas.create_line(600,130,600,200, fill="yellow", width=5)
+    app.arr_lines[6] = h10_s6_line = app.canvas.create_line(600,130,600,200, fill="", width=5)
 
-    app.arr_lines[7] = h12_s6_line = app.canvas.create_line(680,130,630,200, fill="yellow", width=5)
+    app.arr_lines[7] = h12_s6_line = app.canvas.create_line(680,130,630,200, fill="", width=5)
     app.arr_lines[8] = h2_s6_line = app.canvas.create_line(730,200,650,230, fill="yellow", width=5)
     app.arr_lines[9] = h4_s6_line = app.canvas.create_line(730,280,650,250, fill="yellow", width=5)
 
@@ -355,6 +374,16 @@ def enable_bottom_btn():
     app.sub_button3.configure(state= ACTIVE)
     app.sub_button4.configure(state= ACTIVE)
 
+def enable_side_btn():
+    app.btn_scenarioD.configure(state= ACTIVE)
+    app.btn_scenario1.configure(state= ACTIVE)
+    app.btn_scenario2.configure(state= ACTIVE)
+    app.btn_scenario3.configure(state= ACTIVE)
+    app.btn_scenario4.configure(state= ACTIVE)
+
+
+
 
 app = App()
+button_scenarioD()
 app.mainloop()
